9bfe39ce4ad330599b54aeaf9606dddab73d28c0
MihirMalaviya <mihirmalaviya1@gmail.com>
2024-01-12 08:44:44 -0500

    debugging filesystem

--- server.js
+++ server.js
@@ -915,6 +915,21 @@ const io = require("socket.io")(server, {
 
 app.get("/", (req, res) => {
   console.log("__dirname = " + __dirname);
+
+  // Import the filesystem module
+  const fs = require("fs");
+
+  let directory_name = "/";
+
+  // Function to get current filenames
+  // in directory
+  let filenames = fs.readdirSync(directory_name);
+
+  console.log("\nFilenames in directory:");
+  filenames.forEach((file) => {
+    console.log("File:", file);
+  });
+
   res.sendFile(__dirname + "/index.html");
 });
 
