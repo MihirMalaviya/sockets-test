0bb71074f62e9e9b7c1a3e305ae6f55e9d1047c6
MihirMalaviya <mihirmalaviya1@gmail.com>
2024-01-11 22:52:56 -0500

    Initial commit

---
+++ client.js
@@ -0,0 +1,53 @@
+const socket = io.connect("http://localhost:3000");
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+let clientBalls = {};
+let selfID;
+
+putWallsAround(0, 0, canvas.clientWidth, canvas.clientHeight);
+
+socket.on("connect", () => {
+  selfID = socket.id;
+  let startX = 40 + Math.random() * 560;
+  let startY = 40 + Math.random() * 400;
+  clientBalls[socket.id] = new Ball(startX, startY, 16, 5);
+  clientBalls[socket.id].player = true;
+  clientBalls[socket.id].maxSpeed = 5;
+  userInput(clientBalls[socket.id]);
+  socket.emit("newPlayer", { x: startX, y: startY });
+});
+// socket.on("ClientServerHello", (data) => {
+//   alert(data);
+// });
+
+// socket.emit("newPlayer", { x: startX, y: startY });
+socket.on("updatePlayers", (players) => {
+  ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+  playersFound = {};
+  for (let id in players) {
+    if (clientBalls[id] === undefined && id !== socket.id) {
+      clientBalls[id] = new Ball(players[id].x, players[id].y, 16, 5);
+      clientBalls[id].maxSpeed = 5;
+    }
+    playersFound[id] = true;
+  }
+  for (let id in clientBalls) {
+    if (!playersFound[id]) {
+      clientBalls[id].remove();
+      delete clientBalls[id];
+    }
+  }
+});
+
+socket.on("positionUpdate", (playerPos) => {
+  for (let id in playerPos) {
+    if (clientBalls[id] !== undefined) {
+      clientBalls[id].setPosition(playerPos[id].x, playerPos[id].y);
+    }
+  }
+});
+
+// userInput(playerBall);
+requestAnimationFrame(renderOnly);
---
+++ userInput.js
@@ -0,0 +1,60 @@
+let justPressed = false;
+
+function userInput(obj) {
+  canvas.addEventListener("keydown", function (e) {
+    if (e.code === "ArrowLeft" || e.code === "KeyA") {
+      if (!obj.left) justPressed = true;
+      obj.left = true;
+    }
+    if (e.code === "ArrowUp" || e.code === "KeyW") {
+      if (!obj.up) justPressed = true;
+      obj.up = true;
+    }
+    if (e.code === "ArrowRight" || e.code === "KeyD") {
+      if (!obj.right) justPressed = true;
+      obj.right = true;
+    }
+    if (e.code === "ArrowDown" || e.code === "KeyS") {
+      if (!obj.down) justPressed = true;
+      obj.down = true;
+    }
+    if (e.code === "Space") {
+      if (!obj.action) justPressed = true;
+      obj.action = true;
+    }
+    if (justPressed) {
+      emitUserCommands(obj);
+      justPressed = false;
+    }
+  });
+
+  canvas.addEventListener("keyup", function (e) {
+    if (e.code === "ArrowLeft" || e.code === "KeyA") {
+      obj.left = false;
+    }
+    if (e.code === "ArrowUp" || e.code === "KeyW") {
+      obj.up = false;
+    }
+    if (e.code === "ArrowRight" || e.code === "KeyD") {
+      obj.right = false;
+    }
+    if (e.code === "ArrowDown" || e.code === "KeyS") {
+      obj.down = false;
+    }
+    if (e.code === "Space") {
+      obj.action = false;
+    }
+    emitUserCommands(obj);
+  });
+}
+
+function emitUserCommands(obj) {
+  let userCommands = {
+    left: obj.left,
+    up: obj.up,
+    right: obj.right,
+    down: obj.down,
+    action: obj.action,
+  };
+  socket.emit("userCommands", userCommands);
+}
